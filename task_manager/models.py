6ccb9df263184b67ad233bb51f0864f7503e0fd3
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-22 22:31:39 +0200

    Create Worker Position TaskType Task models

--- task_manager/models.py
+++ task_manager/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Worker(AbstractUser):
+    position = models.ForeignKey("Position", on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("UR", 'Urgent'),
+        ("HG", 'High'),
+        ("MD", 'Medium'),
+        ("LW", 'Low'),
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(
+        max_length=2,
+        choices=PRIORITY_CHOICES,
+        default="LW"
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(get_user_model(), related_name="tasks")
+
+    def __str__(self) -> str:
+        return self.name

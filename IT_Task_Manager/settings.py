d0ccacb80960474f995e87dfbaa3e7f7495a81da
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-22 18:56:20 +0200

    Change TIME_ZONE and add crispy_template_pack

--- IT_Task_Manager/settings.py
+++ IT_Task_Manager/settings.py
@@ -68,6 +68,8 @@ TEMPLATES = [
     },
 ]
 
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 WSGI_APPLICATION = "IT_Task_Manager.wsgi.application"
 
 
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Kiev"
 
 USE_I18N = True
 
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

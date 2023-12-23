46549a52b400e88ce9aa916124c415c2f69168c1
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-23 21:03:26 +0200

    add endpoint for index.html

--- IT_Task_Manager/urls.py
+++ IT_Task_Manager/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("task_manager.urls", namespace="task-manager"))
 ]

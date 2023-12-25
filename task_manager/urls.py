cb33216b19d85b9dab7d87d00fda6aa4f4314dc6
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-25 15:38:55 +0200

    Create task-list page add endpoints and navigation button for page

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index
+from .views import index, TaskListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("tasks/", TaskListView.as_view(), name="task-view"),
 ]
 
 app_name = "task-manager"
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from task_manager.models import Task
+
+
+@login_required
+def index(request: HttpRequest) -> HttpResponse:
+    num_workers = get_user_model().objects.count()
+    completed_tasks = Task.objects.filter(is_completed=True).count()
+    num_tasks = Task.objects.count()
+
+    context = {
+        "num_workers": num_workers,
+        "completed_tasks": completed_tasks,
+        "num_tasks": num_tasks,
+    }
+    return render(request, "task_manager/index.html", context=context)
+
+
+class TaskListView(LoginRequiredMixin, generic.ListView):
+    model = Task
+    context_object_name = "task_list"
+    template_name = "task_manager/task_list.html"

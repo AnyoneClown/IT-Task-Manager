15cff3dd370e99c1fca1b4b8a74521fc5bc3abf4
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-25 15:02:25 +0200

    Create logout_redirect_url and endpoints for authentication

--- IT_Task_Manager/settings.py
+++ IT_Task_Manager/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "task_manager",
+    "authentication",
+    "crispy_forms",
 ]
 
 MIDDLEWARE = [
@@ -136,3 +138,4 @@ ASSETS_ROOT = "/static/assets"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/accounts/login/"
--- IT_Task_Manager/urls.py
+++ IT_Task_Manager/urls.py
@@ -3,5 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("task_manager.urls", namespace="task-manager"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("authentication.urls", namespace="authentication")),
+    path("", include("task_manager.urls", namespace="task-manager")),
 ]

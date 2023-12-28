4b58a44202b9ad636dee87e7053f901f6005d918
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-28 17:16:05 +0200

    Initialize .env and add to .gitignore

--- IT_Task_Manager/settings.py
+++ IT_Task_Manager/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-n73=3tg*7yzov^lx*lcfpi$qus-hy3fi6i@og9f0gor)fyhf)="
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

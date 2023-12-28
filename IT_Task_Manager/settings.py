7c3436c37f6699e891f919e03aa3bfa86efdf67d
Denis Dymerlii <dum.denya15@gmail.com>
2023-12-28 22:37:57 +0200

    Add https://it-task-manager-5z1u.onrender.com to allowed hosts

--- IT_Task_Manager/settings.py
+++ IT_Task_Manager/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ["127.0.0.1", ]
+ALLOWED_HOSTS = ["127.0.0.1", "https://it-task-manager-5z1u.onrender.com"]
 
 
 # Application definition
